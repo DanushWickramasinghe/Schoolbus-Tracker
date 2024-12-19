549e8dd0da617ed0a9fa5779c0a3aca0130e6924
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2024-12-19 18:41:09 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import LoginPage from "./pages/login";
-import RegisterPage from "./pages/register";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <RegisterPage />
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
