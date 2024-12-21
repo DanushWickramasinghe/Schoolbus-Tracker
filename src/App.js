70d90027fddf80c3e9ace7565e2636fbf05ff37f
Danush Wickramasinghe <danushwickramasinghe00@gmail.com>
2024-12-21 21:30:31 +0530

    Added vehicle registration page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/commonPages/login";
 import RegisterPage from "./pages/commonPages/register";
 import HomePage from "./pages/commonPages/home";
+import VehicleRegistrationForm from "./pages/userPages/busOwners/vehicleRegister";
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route
+            path="/registervehicle"
+            element={<VehicleRegistrationForm />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/userPages/busOwners/vehicleRegister.js
+++ src/pages/userPages/busOwners/vehicleRegister.js
@@ -1,1 +1,89 @@
 /* Bus owners can register their vehicles here, by filling a form. */
+
+import React from "react";
+import { Form, Input, Button, Select } from "antd";
+
+const { Option } = Select;
+
+const VehicleRegistrationForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Form Submitted:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.error("Form Submission Failed:", errorInfo);
+  };
+
+  return (
+    <div style={{ maxWidth: 600, margin: "0 auto", padding: "50px" }}>
+      <h2 style={{ textAlign: "center" }}>Vehicle Registration</h2>
+      <Form
+        form={form}
+        name="vehicle-registration"
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Name of the owner"
+          name="ownerName"
+          rules={[{ required: true, message: "Please enter the owner's name" }]}
+        >
+          <Input placeholder="Enter owner's full name" />
+        </Form.Item>
+
+        <Form.Item
+          label="NIC Number of the owner"
+          name="NICnumber"
+          rules={[{ required: true, message: "Please enter the NIC number" }]}
+        >
+          <Input placeholder="Enter NIC number" />
+        </Form.Item>
+
+        <Form.Item
+          label="Type of Vehicle"
+          name="vehicleType"
+          rules={[
+            { required: true, message: "Please select the vehicle type" },
+          ]}
+        >
+          <Select placeholder="Select vehicle type">
+            <Option value="van">Van</Option>
+            <Option value="bus - 24 seater">Bus - 24 seater</Option>
+            <Option value="bus - 52 seater">Bus - 52 seater</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label="Vehicle Model"
+          name="vehicleModel"
+          rules={[
+            { required: true, message: "Please enter the vehicle model" },
+          ]}
+        >
+          <Input placeholder="Enter vehicle model" />
+        </Form.Item>
+
+        <Form.Item
+          label="Vehicle Number"
+          name="vehicleNumber"
+          rules={[
+            { required: true, message: "Please enter the vehicle number" },
+          ]}
+        >
+          <Input placeholder="Enter vehicle number" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Register Vehicle
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default VehicleRegistrationForm;
