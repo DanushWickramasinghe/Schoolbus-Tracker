09ad043275dfe8902abf4da8d72bf578961e785c
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2025-01-04 14:17:11 +0530

    create dashboard

--- src/pages/common/dashboard.js
+++ src/pages/common/dashboard.js
@@ -1,49 +1,211 @@
-import React, { useEffect, useState } from "react";
-import { getDashboardData } from "../../apis/admin.api";
+import React, { useState } from 'react';
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Button, Menu } from 'antd';
 
-/* Shows current status of the application, number of users, active users, past users etc. */
-const Dashboard = () => {
-  const [data, setData] = useState({
-    totalUsers: 0,
-    activeUsers: 0,
-    pastUsers: 0,
-    busesOnRoute: 0,
-  });
+const horizontalItems = [
+  {
+    label: 'Navigation One',
+    key: 'mail',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Navigation Two',
+    key: 'app',
+    icon: <AppstoreOutlined />,
+    disabled: true,
+  },
+  {
+    label: 'Navigation Three - Submenu',
+    key: 'SubMenu',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          {
+            label: 'Option 1',
+            key: 'setting:1',
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2',
+          },
+        ],
+      },
+      {
+        type: 'group',
+        label: 'Item 2',
+        children: [
+          {
+            label: 'Option 3',
+            key: 'setting:3',
+          },
+          {
+            label: 'Option 4',
+            key: 'setting:4',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'alipay',
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+  },
+];
 
-  useEffect(() => {
-    try {
-      getDashboardData().then((data) => {
-        setData(data);
-      });
-    } catch (error) {
-      console.error("Error fetching Dashboard data:", error);
-    }
-  }, [setData]);
-  
+const items = [
+  {
+    key: '1',
+    icon: <PieChartOutlined />,
+    label: 'Option 1',
+  },
+  {
+    key: '2',
+    icon: <DesktopOutlined />,
+    label: 'Option 2',
+  },
+  {
+    key: '3',
+    icon: <ContainerOutlined />,
+    label: 'Option 3',
+  },
+  {
+    key: 'sub1',
+    label: 'Navigation One',
+    icon: <MailOutlined />,
+    children: [
+      {
+        key: '5',
+        label: 'Option 5',
+      },
+      {
+        key: '6',
+        label: 'Option 6',
+      },
+      {
+        key: '7',
+        label: 'Option 7',
+      },
+      {
+        key: '8',
+        label: 'Option 8',
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Navigation Two',
+    icon: <AppstoreOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: 'sub3',
+        label: 'Submenu',
+        children: [
+          {
+            key: '11',
+            label: 'Option 11',
+          },
+          {
+            key: '12',
+            label: 'Option 12',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const DashBoard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const [current, setCurrent] = useState('mail');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
 
   return (
-    <div className="dashboard">
-      <h1>Dashboard</h1>
-      <div className="dashboard-stats">
-        <div className="stat">
-          <h2>Total Users</h2>
-          <p>{data.totalUsers}</p>
-        </div>
-        <div className="stat">
-          <h2>Active Users</h2>
-          <p>{data.activeUsers}</p>
-        </div>
-        <div className="stat">
-          <h2>Past Users</h2>
-          <p>{data.pastUsers}</p>
-        </div>
-        <div className="stat">
-          <h2>Buses On Route</h2>
-          <p>{data.busesOnRoute}</p>
-        </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100vw', // Set width to screen width
+        height: '100vh', // Set height to screen height
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          width: '100vw',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Button
+          type='primary'
+          onClick={toggleCollapsed}
+          style={{
+            margin: 8,
+            // marginBottom: 16,
+            width: 40,
+          }}
+        >
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </Button>
+        {/* <h1 style={{margin:3}}>School Bus Tracker</h1> */}
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode='horizontal'
+          items={horizontalItems}
+          style={{
+            display: 'flex',
+            flexGrow: 1, // Make Menu take up remaining space
+            justifyContent: 'flex-end',
+          }}
+          // theme='dark'
+        />
       </div>
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode='inline'
+        theme='dark'
+        inlineCollapsed={collapsed}
+        items={items}
+        style={{
+          flexGrow: 1, // Make Menu take up remaining space
+          minHeight: 0, // Ensure it can shrink if needed
+          maxWidth: 240, // Set max width when collapsed
+
+        }}
+      />
     </div>
   );
 };
-
-export default Dashboard;
+export default DashBoard;
