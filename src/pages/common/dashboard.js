f7dfba75f22a05bed4b1485a98b4acf4991353c8
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2025-01-05 15:03:10 +0530

    dashboard component add

--- src/pages/common/dashboard.js
+++ src/pages/common/dashboard.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import {
   AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
   MailOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   PieChartOutlined,
   SettingOutlined,
+  CarOutlined,
+  UserOutlined,
+  UserSwitchOutlined,
 } from '@ant-design/icons';
 import { Button, Menu } from 'antd';
+
+const General = React.lazy(() => import('../admin/general'));
+const VehicleRegistrationForm = React.lazy(() => import('../user/busOwners/vehicleRegister'));
+
+const componentsMap = {
+  1: <General />,
+  2: <VehicleRegistrationForm />,
+};
 
 const horizontalItems = [
   {
@@ -61,7 +70,7 @@ const horizontalItems = [
   {
     key: 'alipay',
     label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+      <a href='https://ant.design' target='_blank' rel='noopener noreferrer'>
         Navigation Four - Link
       </a>
     ),
@@ -72,70 +81,90 @@ const items = [
   {
     key: '1',
     icon: <PieChartOutlined />,
-    label: 'Option 1',
-  },
-  {
-    key: '2',
-    icon: <DesktopOutlined />,
-    label: 'Option 2',
-  },
-  {
-    key: '3',
-    icon: <ContainerOutlined />,
-    label: 'Option 3',
+    label: 'General',
   },
   {
     key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
+    icon: <CarOutlined />,
+    label: 'Bus',
     children: [
       {
-        key: '5',
-        label: 'Option 5',
-      },
-      {
-        key: '6',
-        label: 'Option 6',
-      },
-      {
-        key: '7',
-        label: 'Option 7',
-      },
-      {
         key: '8',
-        label: 'Option 8',
+        label: 'Register Driver',
+      },
+      {
+        key: '2',
+        label: 'Register Bus',
+      },
+      {
+        key: '9',
+        label: 'View Driver',
+      },
+      {
+        key: '3',
+        label: 'View Bus',
       },
     ],
   },
   {
     key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
+    icon: <UserOutlined />,
+    label: 'Passenger',
     children: [
       {
-        key: '9',
-        label: 'Option 9',
+        key: '4',
+        label: 'Register Passenger',
       },
       {
-        key: '10',
-        label: 'Option 10',
-      },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          {
-            key: '11',
-            label: 'Option 11',
-          },
-          {
-            key: '12',
-            label: 'Option 12',
-          },
-        ],
+        key: '5',
+        label: 'View Passenger',
       },
     ],
   },
+  {
+    key: 'sub3',
+    label: 'Admin',
+    icon: <UserSwitchOutlined />,
+    children: [
+      {
+        key: '6',
+        label: 'Register Admin',
+      },
+      {
+        key: '7',
+        label: 'View Admin',
+      },
+    ],
+  },
+  // {
+  //   key: 'sub2',
+  //   label: 'Navigation Two',
+  //   icon: <AppstoreOutlined />,
+  //   children: [
+  //     {
+  //       key: '9',
+  //       label: 'Option 9',
+  //     },
+  //     {
+  //       key: '10',
+  //       label: 'Option 10',
+  //     },
+  //     {
+  //       key: 'sub3',
+  //       label: 'Submenu',
+  //       children: [
+  //         {
+  //           key: '11',
+  //           label: 'Option 11',
+  //         },
+  //         {
+  //           key: '12',
+  //           label: 'Option 12',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 ];
 
 const DashBoard = () => {
@@ -150,13 +179,15 @@ const DashBoard = () => {
     setCurrent(e.key);
   };
 
+  const [verticalSelected, setVerticalSelected] = useState('1');
+
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column',
-        width: '100vw', // Set width to screen width
-        height: '100vh', // Set height to screen height
+        width: '100vw',
+        height: '100vh',
       }}
     >
       <div
@@ -164,6 +195,7 @@ const DashBoard = () => {
           display: 'flex',
           width: '100vw',
           justifyContent: 'space-between',
+          borderBottom: '1px solid #f0f0f0',
         }}
       >
         <Button
@@ -171,13 +203,14 @@ const DashBoard = () => {
           onClick={toggleCollapsed}
           style={{
             margin: 8,
-            // marginBottom: 16,
             width: 40,
           }}
         >
           {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         </Button>
-        {/* <h1 style={{margin:3}}>School Bus Tracker</h1> */}
+        <h1 style={{ margin: 8, fontSize: 25, marginLeft: 18 }}>
+          School Bus Tracker
+        </h1>
         <Menu
           onClick={onClick}
           selectedKeys={[current]}
@@ -185,26 +218,41 @@ const DashBoard = () => {
           items={horizontalItems}
           style={{
             display: 'flex',
-            flexGrow: 1, // Make Menu take up remaining space
+            flexGrow: 1,
             justifyContent: 'flex-end',
           }}
-          // theme='dark'
         />
       </div>
-      <Menu
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
-        mode='inline'
-        theme='dark'
-        inlineCollapsed={collapsed}
-        items={items}
+      <div
         style={{
-          flexGrow: 1, // Make Menu take up remaining space
-          minHeight: 0, // Ensure it can shrink if needed
-          maxWidth: 240, // Set max width when collapsed
-
+          display: 'flex',
+          flexDirection: 'row',
+          height: '100%',
         }}
-      />
+      >
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          onClick={({ key }) => setVerticalSelected(key)}
+          mode='inline'
+          theme='dark'
+          inlineCollapsed={collapsed}
+          items={items}
+          style={{
+            flexGrow: 1, // Make Menu take up remaining space
+            minHeight: 0, // Ensure it can shrink if needed
+            // width: collapsed ? 80 : 256,
+            maxWidth: collapsed ? 80 : 256,
+          }}
+        />
+        <div style={{ flexGrow: 1, padding: 16 }}>
+          <Suspense fallback={<div>Loading...</div>}>
+            {componentsMap[verticalSelected] || (
+              <p>Please select a valid option.</p>
+            )}
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 };
