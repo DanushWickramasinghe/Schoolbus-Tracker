3f3442118b58c4fce283c5cb4541d64801fec249
Danush Wickramasinghe <105097461+DanushWickramasinghe@users.noreply.github.com>
2025-01-10 22:44:25 +0530

    Changed top Navigation pane

--- src/pages/common/dashboard.js
+++ src/pages/common/dashboard.js
@@ -40,50 +40,49 @@ const componentsMap = {
 
 const horizontalItems = [
   {
-    label: "Navigation One",
-    key: "mail",
-    icon: <MailOutlined />,
+    label: "About Us",
+    key: "aboutus",
+    icon: <UserOutlined />,
   },
   {
-    label: "Navigation Two",
-    key: "app",
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: "Navigation Three - Submenu",
-    key: "SubMenu",
+    label: "Help and Support",
+    key: "helpandsupport",
     icon: <SettingOutlined />,
-    children: [
-      {
-        type: "group",
-        label: "Item 1",
-        children: [
-          {
-            label: "Option 1",
-            key: "setting:1",
-          },
-          {
-            label: "Option 2",
-            key: "setting:2",
-          },
-        ],
-      },
-      {
-        type: "group",
-        label: "Item 2",
-        children: [
-          {
-            label: "Option 3",
-            key: "setting:3",
-          },
-          {
-            label: "Option 4",
-            key: "setting:4",
-          },
-        ],
-      },
-    ],
+  },
+  {
+    label: "Contact Us",
+    key: "contactus",
+    icon: <MailOutlined />,
+    // children: [
+    //   {
+    //     type: "group",
+    //     label: "Item 1",
+    //     children: [
+    //       {
+    //         label: "Option 1",
+    //         key: "setting:1",
+    //       },
+    //       {
+    //         label: "Option 2",
+    //         key: "setting:2",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     type: "group",
+    //     label: "Item 2",
+    //     children: [
+    //       {
+    //         label: "Option 3",
+    //         key: "setting:3",
+    //       },
+    //       {
+    //         label: "Option 4",
+    //         key: "setting:4",
+    //       },
+    //     ],
+    //   },
+    // ],
   },
   {
     label: "Logout",
