4858411b048ad8c4033e8f43f5933722b93a927c
Danush Wickramasinghe <105097461+DanushWickramasinghe@users.noreply.github.com>
2025-01-13 23:33:20 +0530

    Commented the usage of General page of the dashboard.

--- src/pages/admin/general.js
+++ src/pages/admin/general.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { getDashboardData } from "../../apis/admin.api";
 import { Card, Col } from "antd";
 
+// Here this component should be implemented further, to display a Map with the location of the buses.
+// Admin should be able to see the locations of all the registered buses on the map.
+// Each passenger should be able to see the location of the bus they are traveling in.
+
 const General = () => {
   const [userCount, setUserCount] = useState(0);
 
