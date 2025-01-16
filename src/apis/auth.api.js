95c9749fbce315023aba1ae9698129e8c820508c
Danush Wickramasinghe <105097461+DanushWickramasinghe@users.noreply.github.com>
2025-01-16 23:00:34 +0530

    Commented authentication

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
-import Host from '../configs/server';
+import axios from "axios";
+import Host from "../configs/server";
 
+// Contains all the APIs related to authentication.
 export const login = async (userData) => {
   try {
     const response = await axios.post(`${Host}/api/auth/login`, userData);
-    localStorage.setItem('accessToken', response.data.accessToken);
-    localStorage.setItem('refreshToken', response.data.refreshToken);
-    localStorage.setItem('user', JSON.stringify(response.data.user));
+    localStorage.setItem("accessToken", response.data.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken);
+    localStorage.setItem("user", JSON.stringify(response.data.user));
     return response.data;
   } catch (error) {
     console.error(error.response.data.message);
@@ -16,14 +17,14 @@ export const login = async (userData) => {
 
 export const getAccessTokenWithRefreshToken = async () => {
   try {
-    const refreshToken = localStorage.getItem('refreshToken');
+    const refreshToken = localStorage.getItem("refreshToken");
     const response = await axios.get(`${Host}/api/auth/access-token`, {
       headers: {
         Authorization: `Bearer ${refreshToken}`,
       },
     });
-    localStorage.setItem('accessToken', response.data.accessToken);
-    localStorage.setItem('refreshToken', response.data.refreshToken);
+    localStorage.setItem("accessToken", response.data.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken);
     return response.data;
   } catch (error) {
     return error;
@@ -32,28 +33,28 @@ export const getAccessTokenWithRefreshToken = async () => {
 
 export const logout = () => {
   try {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    localStorage.removeItem('user');
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("user");
   } catch (error) {
-    console.error('Error logging out:', error);
+    console.error("Error logging out:", error);
   }
 };
 
 export const registerData = async (userData) => {
   try {
     const response = await axios.post(`${Host}/api/auth/register`, userData);
-    localStorage.setItem('email', response.data.email);
+    localStorage.setItem("email", response.data.email);
     return response.data;
   } catch (error) {
-    console.error('Error registering:', error);
+    console.error("Error registering:", error);
     return error.response.data.message;
   }
 };
 
 export const verifyRegisterOtp = async (otpPasswordData) => {
   const compltedData = {
-    email: localStorage.getItem('email'),
+    email: localStorage.getItem("email"),
     otp: otpPasswordData.otp,
     password: otpPasswordData.password,
   };
@@ -64,7 +65,7 @@ export const verifyRegisterOtp = async (otpPasswordData) => {
     );
     return response.data;
   } catch (error) {
-    console.error('Error verifying OTP:', error);
+    console.error("Error verifying OTP:", error);
     return error;
   }
 };
